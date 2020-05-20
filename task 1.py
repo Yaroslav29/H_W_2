0199a965494bf3a674f363aff89d432bf596052d
Yaroslav29 <65343266+Yaroslav29@users.noreply.github.com>
2020-05-20 22:36:21 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,26 @@
+zen = """"The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.   
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+print ("repeated of better:",zen.count("better"))
+print ("repeated of never:" , zen.count("never"))
+print ("repeated of is:" , zen.count("is"))
+print ("all capital letters",zen.upper())
+print ("replace", zen.replace("i","&"))
